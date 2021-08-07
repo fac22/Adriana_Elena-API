21aaeaa89cce2f944efc72e589e19f28544ef724
Elena <rhapsodyenmauve@gmail.com>
2021-08-07 02:50:27 +0100

    template with country results after user input, the results for the requested country appear under the search bar

--- script.js
+++ script.js
@@ -5,16 +5,36 @@ form.addEventListener('submit', (e) => {
 
   const formData = new FormData(form);
   const data = formData.get('country');
-  
-});
 
-fetch('https://api.covid19api.com/live/country/south-africa')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Error in retrieving country data!');
-    }
-    return response.json();
-  })
-  .then((array) => console.log(array[array.length - 1]))
-  .then(console.log)
-  .catch(console.error);
+  //   -------------- API request used: GET 'Live By Country All Status
+  fetch(`https://api.covid19api.com/live/country/${data}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error in retrieving country data!');
+      }
+      return response.json();
+    })
+    .then((array) => {
+      if (array.length !== 0) {
+        return array[array.length - 1];
+      } else throw new Error('Please enter a country!');
+    })
+    // .then(console.log)
+    .then((countryData) => {
+      const attach = document.querySelector('#attach');
+      const template = document.querySelector('#country-results');
+      const domFragment = template.content.cloneNode(true);
+
+      domFragment.querySelector('#searched-country').textContent =
+        countryData.Country;
+      domFragment.querySelector('#confirmed').textContent =
+        countryData.Confirmed;
+      domFragment.querySelector('#deaths').textContent = countryData.Deaths;
+      domFragment.querySelector('#recovered').textContent =
+        countryData.Recovered;
+      domFragment.querySelector('#active').textContent = countryData.Active;
+
+      attach.appendChild(domFragment);
+    })
+    .catch(console.error);
+});
